c3743304fbd2f0ffbd082538f6f3897c45a6dc20
hiroyuki-seto <hiswing8@gmail.com>
2022-01-07 21:37:45 +0900

    add q3,q4

---
+++ chapter1/question3.js
@@ -0,0 +1,4 @@
+// nが負の整数の場合の[n % 2]の答えは負数になる確認
+const n = -2
+const result = n % 2
+console.log(result) // -1
---
+++ chapter1/question5.js
@@ -0,0 +1,12 @@
+const backslash1 = "\\\\"
+console.log(backslash1)
+
+const backslash2 = '\\\\'
+console.log(backslash2)
+
+const backslashValue = '\\'
+console.log(`${backslashValue}${backslashValue}`)
+
+const backslash3 = '\u{005C}\u{005C}'
+console.log(backslash3)
+
